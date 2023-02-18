2b2148c64786fb56aa87be41ea722a0f6577b0b2
jgporter24 <110738676+jgporter24@users.noreply.github.com>
2023-02-18 15:57:49 -0600

    updated connection

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
-const userRoutes = require('./user-routes');
-const thoughtRoutes = require('./thought-routes');
+const userRoutes = require('./user-routes.js');
+const thoughtRoutes = require('./thought-routes.js');
 
 router.use('/users', userRoutes);
 
