2a52f652c39baef8a6974c978903c312b2db780d
jgporter24 <110738676+jgporter24@users.noreply.github.com>
2023-02-18 18:44:07 -0600

    updated syntax

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
-const userRoutes = require('./user-routes.js');
-const thoughtRoutes = require('./thought-routes.js');
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
 router.use('/users', userRoutes);
 
